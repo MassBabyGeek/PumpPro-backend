c0ff941027cee7085c0cdbf3e644236b558ebae8
MassBabyGeek <l.usereau@outlook.fr>
2025-10-08 00:37:28 +0200

    Add print on responses

--- internal/utils/response.go
+++ internal/utils/response.go
@@ -2,7 +2,9 @@ package utils
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"reflect"
 )
 
 type APIResponse struct {
@@ -18,14 +20,18 @@ func JSON(w http.ResponseWriter, status int, payload interface{}) {
 	json.NewEncoder(w).Encode(payload)
 }
 
+// Success affiche le type et la valeur de data dans la console
 func Success(w http.ResponseWriter, data interface{}) {
+	fmt.Printf("[INFO][Success] Type: %s, Value: %#v\n", reflect.TypeOf(data), data)
 	JSON(w, http.StatusOK, APIResponse{Success: true, Data: data})
 }
 
 func Error(w http.ResponseWriter, status int, err string) {
+	fmt.Printf("[ERROR][%d] %s\n", status, err)
 	JSON(w, status, APIResponse{Success: false, Error: err})
 }
 
 func Message(w http.ResponseWriter, msg string) {
+	fmt.Printf("[INFO][MESSAGE] %s\n", msg)
 	JSON(w, http.StatusOK, APIResponse{Success: true, Message: msg})
 }
